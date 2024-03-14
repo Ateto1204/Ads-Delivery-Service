a3ba9029ac1adf2bbaa04491b60b380a766e6642
Ateto1202 <tonysu1204@gmail.com>
2024-03-14 17:19:49 +0800

    Create the testing script

---
+++ assignment/curl example.bash
@@ -0,0 +1,17 @@
+curl -X POST -H "Content-Type: application/json" \
+  http://localhost:8080/api/v1/ad \
+  --data '{
+     "title": "AD 55",
+     "startAt": "2023-12-10T03:00:00.000Z",
+     "endAt": "2023-12-31T16:00:00.000Z",
+     "conditions": {
+        "ageStart": 20,
+        "ageEnd": 30,
+        "country": ["TW", "JP"],
+        "platform": ["android", "ios"]
+     }  
+  }'
+
+
+curl -X GET -H "Content-Type: application/json" \
+  "http://localhost:8080/api/v1/ad?offset=10&limit=3&age=24&gender=F&country=TW&platform=ios"
